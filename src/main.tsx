aa7a116ba932094b565f961842e36f1c831745ca
sebas <sebas@pop-os.localdomain>
2023-05-29 23:56:49 -0400

    Demo de landing

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Landing from "./views/Landing";
+import Login from "./views/Login";
+import Signin from "./views/Signin";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<div/>}>
-    </Route>
-  )
+    <Route path="/" element={<Landing />}>
+      <Route path="login" element={<Login />} />
+      <Route path="signin" element={<Signin />} />
+    </Route>,
+  ),
 );
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
