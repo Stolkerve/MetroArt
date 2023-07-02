1b5e142cd407b5e0a2817d47b3dd5be593c277de
Andres Bucarello <andres.bucarello@correo.unimet.edu.ve>
2023-07-02 19:12:23 -0400

    Cambio FeedbackCard

--- src/components/FeedbackCard.tsx
+++ src/components/FeedbackCard.tsx
@@ -3,9 +3,7 @@ import React from "react";
 export const FeedbackCard = () => {
   return `<div class="contenedor">
   <header class="header">
-      <div class="User">
-          <img src="" alt="Imagen del usuario"></img><h3 class="User" id="username">nombreusuario</h3>
-      </div>   
+        <img src="" alt="Imagen del usuario"></img><h3 class="User" id="username">nombreusuario</h3>
   </header>
   <main class="content"> 
       <section class="Mensaje">
