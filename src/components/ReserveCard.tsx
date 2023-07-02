4e1da077f8a4918cb0704ee20d864cccf8d96798
Andres Bucarello <andres.bucarello@correo.unimet.edu.ve>
2023-07-02 19:12:31 -0400

    Create ReserveCard.tsx

---
+++ src/components/ReserveCard.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const ReserveCard = () => {
+  return (<div className="contenedor">
+  <aside className="left">
+      <img src="" alt="Imagen del tour"></img>
+  </aside>
+  <main className="content"> 
+      <section className="TourInfo">
+          <h3 className="text-center text-white text-4xl mb-10 font-semibold">TituloTour</h3> 
+          <h3 id="descripcion">DescripcionTour</h3>
+      </section>
+      <section className="footer">
+          <h3>Fecha</h3>
+          <button type="submit" className="btn-primary">
+              Feedback
+          </button>
+      </section>
+  </main>
+</div>);
+};
