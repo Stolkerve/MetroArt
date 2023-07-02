eb89eaea6404728dfed9bbbbb4d623aab65ccd69
Stolkerve <devsebasgr@gmail.com>
2023-07-01 22:14:53 -0400

    Arreglar rutas

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Landing from "./views/Landing";
-import { Login } from "./views/Login";
-import { dashboardURL, homeURL, loginURL, registerURL } from "./constants/urls";
-import { Register } from "./views/Register";
-import { Dashboard } from "./views/Dashboard";
-import { Layout } from "./views/Layout";
+import { BrowserRouter } from "react-router-dom";
+import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path={homeURL} element={<Landing />} />
-        <Route path={registerURL} element={<Register />} />
-        <Route path={loginURL} element={<Login />} />
-        
-        <Route element={<Layout />} >
-          <Route path={dashboardURL} element={<Dashboard />} />
-        </Route>
-      </Routes>
+      <App/>
     </BrowserRouter>
   </React.StrictMode>,
 );
