635bc09f58ff64190de824884fe076b7b29f732e
Rolando Sorrentino <rolando.sorrentino@correo.unimet.edu.ve>
2023-07-03 00:56:46 -0400

    Calendar fixes

--- src/components/Calendario.tsx
+++ src/components/Calendario.tsx
@@ -4,12 +4,12 @@ import Calendar from 'react-calendar';
 export const Calendario = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
 
-  const handleDateChange = (date) => {
+  const handleDateChange = (date: any) => {
     setSelectedDate(date);
   }
 
   return (
-    <div>
+    <div className="flex justify-center min-h-screen min-w-screen mt-10 h-full w-full">
       <h2>Selecciona una fecha:</h2>
       <Calendar
         onChange={handleDateChange}
